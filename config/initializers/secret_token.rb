93604531fb477bfe23d44f4edb93dd48d822acc7
zhangcaiyan <zhangcaiyanbeyond@gmail.com>
2012-09-24 14:58:09 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocomotiveCms::Application.config.secret_token = '44e5e1cc4ec3cd263176636692f33f47b6825c36a2b9be2b8344a8cf681cb2318ee5f2cbf51969e67d0398628c361afc4c8d01749b57cd77557f0ab71eee17a2'
